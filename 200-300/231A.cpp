9fac089153dd11d66a2d573698621c8df7363be3
Luc Wallace <lucwallace06@gmail.com>
2025-07-11 21:50:16 +0100

    Add solution for 231A

--- 200-300/231A.cpp
+++ 200-300/231A.cpp
@@ -6,5 +6,20 @@ int main() {
   ios::sync_with_stdio(0);
   cin.tie(0);
 
-  
+  int total = 0;
+  int n;
+  cin >> n;
+
+  while (n--) {
+    bool p;
+    bool v;
+    bool t;
+    cin >> p >> v >> t;
+
+    if (p && v || p && t || v && t) {
+      total++;
+    }
+  }
+
+  cout << total << '\n';
 }
